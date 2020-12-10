293dd8434cd9fab735d7ed871c073ac16ac0d79a
fdz23 <ferdavid_9@hotmail.com>
2020-12-10 18:20:17 -0300

    Create Day1 solution

---
+++ AdventOfCode2020/Solutions/Day1.cs
@@ -0,0 +1,31 @@
+﻿using AOC2020.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AOC2020.Solutions
+{
+    public class Day1
+    {
+        private readonly FileParser _fileParser = new FileParser();
+
+        public int Part1()
+        {
+            var input = _fileParser.ParseFile($"Day1.txt");
+            var solution = 0;
+
+            foreach (var number in input)
+            {
+                solution = number;
+                var otherNumber = input.Find(n => n + number == 2020);
+
+                if (otherNumber != 0)
+                {
+                    return solution * otherNumber;
+                }
+            }
+
+            return solution;
+        }
+    }
+}
