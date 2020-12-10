cc0022398e36278b2724215f8f3909fa01c5aef8
fdz23 <ferdavid_9@hotmail.com>
2020-12-10 20:04:32 -0300

    Create DTO for Day2

---
+++ AdventOfCode2020/DTOs/Day2Part1DTO.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020.DTOs
+{
+    public class Day2Part1DTO
+    {
+        public int Min { get; set; }
+        public int Max { get; set; }
+        public string Character { get; set; }
+        public string Password { get; set; }
+    }
+}
