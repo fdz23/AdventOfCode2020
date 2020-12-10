17b1b503bac0526d06eb1ba959ab2fcbb612705c
fdz23 <ferdavid_9@hotmail.com>
2020-12-10 20:04:42 -0300

    Create extensions

---
+++ AdventOfCode2020/Utils/Extensions.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode2020.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020.Utils
+{
+    public static class Extensions
+    {
+        public static Day2Part1DTO ConvertDay2Part1DTO(this string line)
+        {
+            var lineSplit = line.Split(" ");
+            var numbers = lineSplit[0].Split("-");
+
+            return new Day2Part1DTO
+            {
+                Character = lineSplit[1].Split(":")[0],
+                Min = Convert.ToInt32(numbers[0]),
+                Max = Convert.ToInt32(numbers[1]),
+                Password = lineSplit[2]
+            };
+        }
+
+        public static bool IsBetween(this int number, int min, int max)
+        {
+            return number >= min && number <= max;
+        }
+    }
+}
