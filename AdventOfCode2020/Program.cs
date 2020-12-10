5ab7e1881820ff113144f3015a41cc12096aca0a
fdz23 <ferdavid_9@hotmail.com>
2020-12-10 18:20:25 -0300

    Create main method

---
+++ AdventOfCode2020/Program.cs
@@ -0,0 +1,13 @@
+﻿using AOC2020.Solutions;
+using System;
+
+namespace AOC2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Day 1 Part 1 : {new Day1().Part1()}");
+        }
+    }
+}
