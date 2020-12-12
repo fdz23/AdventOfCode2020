b685a6cbc11b566db99c157bcb6cfa33645d0814
fdz23 <ferdavid_9@hotmail.com>
2020-12-12 16:30:30 -0300

    Create passport DTO

---
+++ AdventOfCode2020/DTOs/PassportDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020.DTOs
+{
+    public class PassportDTO
+    {
+        public Dictionary<string, string> Fields { get; set; }
+
+        public bool IsValid { get; set; }
+    }
+}
