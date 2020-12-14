3d0bcf72eb7cd22f187c3064b986a5c5a37d6b47
fdz23 <ferdavid_9@hotmail.com>
2020-12-14 20:10:03 -0300

    Create Business Object for Day5

---
+++ AdventOfCode2020/Data/Seat.cs
@@ -0,0 +1,70 @@
+﻿using AdventOfCode2020.Utils;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020.Data
+{
+    public class Seat
+    {
+        public Seat()
+        {
+            MinRow = 0;
+            MaxRow = 127;
+            MinCol = 0;
+            MaxCol = 7;
+        }
+
+        public int MinRow { get; set; }
+
+        public int MaxRow { get; set; }
+
+        public int MinCol { get; set; }
+
+        public int MaxCol { get; set; }
+
+        public long Id => MinRow * 8 + MaxCol;
+
+        public void CalculateSeatId(string line)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                switch (line.CharacterAt(i))
+                {
+                    case "B":
+                        GetUpperHalfRow();
+                        break;
+                    case "F":
+                        GetLowerHalfRow();
+                        break;
+                    case "R":
+                        GetUpperHalfCol();
+                        break;
+                    case "L":
+                        GetLowerHalfCol();
+                        break;
+                }
+            }
+        }
+
+        private void GetUpperHalfRow()
+        {
+            MinRow += (MaxRow - MinRow) / 2 + 1;
+        }
+
+        private void GetLowerHalfRow()
+        {
+            MaxRow = MinRow + (MaxRow - MinRow) / 2;
+        }
+
+        private void GetUpperHalfCol()
+        {
+            MinCol += (MaxCol - MinCol) / 2 + 1;
+        }
+
+        private void GetLowerHalfCol()
+        {
+            MaxCol = MinCol + (MaxCol - MinCol) / 2;
+        }
+    }
+}
--- AdventOfCode2020/Solutions/Day5.cs
+++ AdventOfCode2020/Solutions/Day5.cs
@@ -1,4 +1,5 @@
-﻿using AdventOfCode2020.DTOs;
+﻿using AdventOfCode2020.Data;
+using AdventOfCode2020.DTOs;
 using AdventOfCode2020.Utils;
 using System;
 using System.Collections.Generic;
@@ -11,70 +12,35 @@ namespace AdventOfCode2020.Solutions
     {
         private readonly FileParser _fileParser = new FileParser();
         private readonly List<string> _input;
-        private readonly List<long> _idList;
+        private readonly List<Seat> _seats;
 
         public Day5()
         {
             _input = _fileParser.ParseFile("Day5.txt");
-            _idList = new List<long>();
+            _seats = new List<Seat>();
         }
 
         public long Part1()
         {
-            long maxSeatId = 0;
+            var maxSeatId = new Seat
+            {
+                MaxCol = 0
+            };
 
             foreach (var line in _input)
             {
-                var seatId = DiscoverSeatId(line);
-                _idList.Add(seatId);
+                var seat = new Seat();
 
-                if (seatId > maxSeatId)
+                seat.CalculateSeatId(line);
+                _seats.Add(seat);
+
+                if (seat.Id > maxSeatId.Id)
                 {
-                    maxSeatId = seatId;
+                    maxSeatId = seat;
                 }
             }
 
-            return maxSeatId;
-        }
-
-        public long DiscoverSeatId(string line)
-        {
-            int minCol = 0;
-            int maxCol = 7;
-            int maxRow = 127;
-            int minRow = 0;
-
-            for (int i = 0; i < line.Length; i++)
-            {
-                if (line.CharacterAt(i).Equals("B"))
-                {
-                    minRow += GetUpperHalf(minRow, maxRow);
-                }
-                if (line.CharacterAt(i).Equals("F"))
-                {
-                    maxRow = GetLowerHalf(minRow, maxRow);
-                }
-                if (line.CharacterAt(i).Equals("R"))
-                {
-                    minCol += GetUpperHalf(minCol, maxCol);
-                }
-                if (line.CharacterAt(i).Equals("L"))
-                {
-                    maxCol = GetLowerHalf(minCol, maxCol);
-                }
-            }
-
-            return minRow * 8 + maxCol;
-        }
-
-        private int GetUpperHalf(int min, int max)
-        {
-            return (max - min) / 2 + 1;
-        }
-
-        private int GetLowerHalf(int min, int max)
-        {
-            return min + (max - min) / 2;
+            return maxSeatId.Id;
         }
     }
 }
