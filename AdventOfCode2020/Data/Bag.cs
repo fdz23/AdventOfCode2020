fd59d63488a7aefcfdfd8bfa62b1c05bca1291b8
fdz23 <ferdavid_9@hotmail.com>
2020-12-16 22:38:31 -0300

    Create Bag class

---
+++ AdventOfCode2020/Data/Bag.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2020.Data
+{
+    public class Bag
+    {
+        public Bag()
+        {
+            Bags = new Dictionary<Bag, int>();
+        }
+
+        public string Color { get; set; }
+
+        public Dictionary<Bag, int> Bags { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Bag bag && Equals(Color, bag.Color);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Color, Bags);
+        }
+    }
+}
