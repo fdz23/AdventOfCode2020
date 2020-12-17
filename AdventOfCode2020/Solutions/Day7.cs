fb7c304ecd88695818a9640da60ebd187e67d1b3
fdz23 <ferdavid_9@hotmail.com>
2020-12-16 22:38:50 -0300

    Create Day7 part1 solution

---
+++ AdventOfCode2020/Solutions/Day7.cs
@@ -0,0 +1,96 @@
+﻿using AdventOfCode2020.Data;
+using AdventOfCode2020.Utils;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AdventOfCode2020.Solutions
+{
+    public class Day7
+    {
+        private readonly FileParser _fileParser = new FileParser();
+        private readonly List<string> _input;
+        private readonly List<Bag> _bags = new List<Bag>();
+
+        public Day7()
+        {
+            _input = _fileParser.ParseFile("Day7.txt");
+            CreateBags();
+        }
+
+        public long Part1()
+        {
+            long bagCount = 0;
+
+            foreach (var bag in _bags.Where(b => !b.Color.Equals("shiny gold")))
+            {
+                bagCount += GetSingleBagCount(bag, "shiny gold");
+            }
+
+            return bagCount;
+        }
+
+        private void CreateBags()
+        {
+            CreateBagsWithoutReference();
+            CreateReferenceForBags();
+        }
+
+        private void CreateBagsWithoutReference()
+        {
+            _bags.AddRange(_input.Select(GetBagFromLine));
+        }
+
+        private void CreateReferenceForBags()
+        {
+            foreach (var bag in _bags)
+            {
+                foreach (var line in _input.Where(l => !l.Contains("no other bags") && l.StartsWith(bag.Color)))
+                {
+                    var contained = line.Split("contain ")[1];
+                    var differentBags = contained.Split(", ");
+
+                    foreach (var differentBag in differentBags)
+                    {
+                        var containedBag = GetBagFromLine(differentBag);
+                        bag.Bags.Add(_bags.FirstOrDefault(b => b.Equals(containedBag)), differentBag.Split(" ")[0].ToInt().Value);
+                    }
+                }
+            }
+        }
+
+        private Bag GetBagFromLine(string line)
+        {
+            var words = line.Split(" ");
+            var color = words[0].ToInt() == null ? $"{words[0]} {words[1]}" : $"{words[1]} {words[2]}";
+
+            return new Bag
+            {
+                Color = color
+            };
+        }
+
+        private long GetSingleBagCount(Bag obj, string colorToFind)
+        {
+            long bagCount = 0;
+
+            if (obj.Color.Equals(colorToFind))
+            {
+                return 1;
+            }
+            else
+            {
+                foreach (var bag in obj.Bags.Keys)
+                {
+                    if (GetBagCount(bag, colorToFind) > 0)
+                    {
+                        return 1;
+                    }
+                }
+            }
+
+            return bagCount;
+        }
+    }
+}
